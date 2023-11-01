aedcf1f4d3fc47cb2fca257af2ef27bdbfcbd0aa
Lucas Alves <lucasantunesalves12@hotmail.com>
2023-11-01 14:28:27 -0300

    primeiras classes

---
+++ src/Campeonato.java
@@ -0,0 +1,23 @@
+
+public class Campeonato {
+    private Jogador[] jogadores;
+
+    public void incluirJogador() {
+    }
+
+    public void removerJogador() {
+    }
+
+    public void iniciarCampeonato() {
+    }
+
+    public void mostrarCartela() {
+    }
+
+    public void gravarEmArquivo() {
+    }
+
+    public void lerDoArquivo() {
+    }
+
+}
---
+++ src/usaCampeonato.java
@@ -0,0 +1,59 @@
+import java.util.Scanner;
+
+public class usaCampeonato {
+    public static void main(String[] args){
+        Scanner teclado = new Scanner(System.in);
+        char opcao;
+        Campeonato c = new Campeonato();
+        do {
+            System.out.println("..:: Menu interativo ::..");
+            System.out.println("a - Incluir jogador");
+            System.out.println("b - Remover jogador");
+            System.out.println("c - Executar rodada de apostas");
+            System.out.println("d - Imprimir saldo dos jogadores");
+            System.out.println("e - Imprimir extrato dos resultados");
+            System.out.println("f - Imprimir estatísticas");
+            System.out.println("g - Gravar em arquivo");
+            System.out.println("h - Ler de arquivo");
+            System.out.println("i - Sair");
+            System.out.println("Entre com uma opcao do menu: ");
+            opcao = teclado.nextLine().charAt(0);
+
+            switch (opcao) {
+                case 'a':
+                    c.incluirJogador();
+                    break;
+                case 'b':
+                    c.removerJogador();
+                    break;
+                case 'c':
+                    c.iniciarCampeonato();
+                    break;
+                case 'd':
+                    c.mostrarCartela();
+
+                    break;
+                case 'e':
+                    
+                    break;
+                case 'f':
+
+                    break;
+                case 'g':
+                    c.gravarEmArquivo();
+                    break;
+                case 'h':
+                    c.lerDoArquivo();
+                    break;
+                case 'i':
+                    System.out.println("Saindo...");
+                break;
+                default:
+                    System.out.println("Opcao invalida. Tente novamente");
+            }
+        } while (opcao != 'i');
+
+        teclado.close();
+    }
+}
+
