5d436a1bb80e86d9dfd5fcf1b808028ce074e766
Pedro marafiga <pmarafiga15@gmail.com>
2023-11-06 15:24:48 -0300

    classe campeonato alterada

--- src/Campeonato.java
+++ src/Campeonato.java
@@ -1,6 +1,11 @@
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 
 public class Campeonato {
     private Jogador[] jogadores;
+    private int qtdJogadores;
 
     public void incluirJogador() {
     }
@@ -14,10 +19,49 @@ public class Campeonato {
     public void mostrarCartela() {
     }
 
+     //grava em arquivo .dat a rodada executada
     public void gravarEmArquivo() {
+        try {
+            FileOutputStream fout = new FileOutputStream(arq);
+            ObjectOutputStream oos = new ObjectOutputStream(fout);
+            // gravando o vetor de pessoas no arquivo
+            oos.writeObject(jogadores);
+            oos.flush();
+            oos.close();
+            fout.close();
+
+            System.out.println("Dados gravados com sucesso!\n");
+        } catch (Exception ex) {
+            System.err.println("erro: " + ex.toString());
+        }
+
     }
 
+    //le os dados do arquvo .dat
     public void lerDoArquivo() {
+        try {
+            FileInputStream fin = new FileInputStream(arq);
+            ObjectInputStream oin = new ObjectInputStream(fin);
+
+            // Lendo os objetos de um arquivo e fazendo a coercao de tipos
+
+            Jogador[] jogaArq = (Jogador[]) oin.readObject();
+            oin.close();
+            fin.close();
+            jogadores = jogaArq;
+            qtdJogadores = 0;
+            
+            for(Jogador j : jogadores) {
+                if(j != null) {
+                    qtdJogadores++;
+                }
+            }
+
+            System.out.println("Dados lidos com sucesso!\n");
+        } catch (Exception ex) {
+            System.err.println("erro: " + ex.toString());
+        }
+
     }
 
 }
--- src/JogoAzar.java
+++ src/JogoAzar.java
@@ -5,4 +5,10 @@ public class JogoAzar extends JogoDados{
         this.valorAposta = valorAposta;
     }
 
+    @Override
+    public int somarFacesSorteadas(Dado[] dados) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'somarFacesSorteadas'");
+    }
+
 }
